07f753e8c25fd73f5b8f0d69c84ee9da44279afa
fish <1464250028@qq.com>
2018-02-26 15:14:47 +0800

    '陆金所登录添加验证验证码正确性再登录的步骤'

--- lujinsuo_login/ljs.py
+++ lujinsuo_login/ljs.py
@@ -28,7 +28,7 @@ data = {
 resp = session.post(accept_device_url, headers=headers, data=data)
 print resp.text
 
-# ===========================================================
+# ======================= 获取验证码 ====================================
 get_captcha_url = 'https://user.lu.com/user/captcha/captcha.jpg'
 params = {
     'source':'login',
@@ -40,27 +40,37 @@ with open('captcha.jpg', 'wb') as f:
 
 captcha = raw_input(u'请输入验证码:')
 
-
-# ==============================================================
-login_url = 'https://user.lu.com/user/login'
-data = {
-    'isTrust':'Y',
-    'password':rsa_encrypt(password),
-    'openlbo':'0',
-    'deviceKey':'1DA04D5FA06A3DF9351172402D3736E93F5AD7BB4422E6181D17A9C9FE55ABD323517C5B01B09D1254E255600623E57D294B58C7CDAEF4FAA973316CFCAA9CE4E8DD889CF3874E1E9AFFF57FAED8789446D8C69DED5D2EDBAFC73A3B262BF9ED060D16D364AC30720DF3B707A1E81DC89D36822E679D61A3B732A28A02D84678',
-    'deviceInfo':'ALxyJ/Hzq1cZXHj+WaUCFi8QMlvz0AkQLB8PE6s8VkmRvQy8v9ALWBjtK6pMCjjElX38kes1jGTKpuNvCqZdwQTDF2YVkoL53zgkMEsgO95ZZV1knicKDZ47IZYGXn9neALil2mOa1uOhco5IRMpHT3dJ+JMNg3L1rNEqAGqoX5sQQ51R/R68T22iAMFll6bXUTJTM6Qqlp8ZtijSDwADPEr2bCSlgvmnVcmPdXHXL3nmufjhJ9p7Y44ZuJ/X8MXg6TD77Gdl0DqXd4j70V8X0REudlB4mmvm0uUI8u/kJbjvLLMmpn8lz55bVxHEcFmncEadDFHqYqz31pVb32N4y7/a4bYw5ccPuV6MWX/ZQcdOwGMKQuIvBbbhv70sZZgaO3Qcksp57oPYZFs1zOKYTQDPHHPCmm8HSMq8oauUUfsquo02hA9a47t6A0lDAxuTEQK5RAkdNA3xJyTWfIWqgsDKiRyDQt51jxIZN4DQceUbZBu68xPABxqHUECt07/hiLBrWrQE2O1XzFt3D7WtIMHmAsMe1x/EaLugBMOv7mw4s5cbAjGrL1c5SqfFiTod3NJw2Bo8ZvXMWkpYIV6RtIyvAEaOeY1WMIb+zGei03ZN3Cdwut7mFJ230sPN15QAErZ5Awtax4JUvq4eG2GZoGIUj/sE7Xy7YVhN7p3XGzu+HJw/FJjNBcitE6Mrp8tluTOAOgzcOhFeLEimsAbOTgSyKBaZHZFpJRmRB4WLE0Qh4sQza9jAW+CY7viEF7tRtvkz1pUCjkgFx1al6NxzI39z4iAXrWNZfbsan/9phM27duOeWj6XBXVPgXrtt31LXKCzoSu7GO2ZHxOGRaIi9WKn/FCJkgEYLheORcQuw827duOeWj6XBXVPgXrtt31k7J96vyzYUJtNRgXg47ryaEJnrowXYSOQdhwWm76GvPOSkF31CMkomVId9lumzUSLwb8ABAARkRra79AW/BkAbAGyMZBxFso4AEny9fZnBZpVAuN96/RblkcowPCm5efJX4o+VuMuh7VRvObZFiLdSE4k0yrbPL1UoUNZP4JY4OYzkiMR70mGgxVHLN/vuXyDDo6M8u+96Ch6BkQuYh4jVj/9lwXYMXNbMQwnchIL+QNsZz4slht4C3+gRSQfIZn+TBQnaVv+7BeUIaCW+nIqgvrbG0p5N2X6D/gkq7mN28xReqmLGr4FY60wKtF4PXXHC9qSFkeIBVWg79pGVMG73BTHOTFpcaTKNgK4br7EYgCFjZaUnmsxdQUc5v/KUOM/MYNXC4Jv7cOlA8AtpBZnvco2H1f6IiHp4Z9GtJiHhqXMYUls6CjBFtpyjxk0uVCl0cuoE8CwCPYZWMg1sZz7Ceib5BdMxUTi0gTV7MzMpZXrlQdqdhJKKE1qZD7TE1SEqkzoPv2ll/WZG7juiVIbx9T/ey62uzRIQZXCo5CSqgxReqmLGr4FY60wKtF4PXXOI0qe0YiUYhkEHu+rkAO4aogTj3OKghzLyqw5Bj9qUeNOCQfsZ5fxZ3iZs/4nJRo8KdA3e1vXduJUIWVe3uCyDPf1+pMAsYjiHli7oJWd/ETeHQAAP5fiRiA9JP5nzbyE9EsspJJd2VZnChkkUKdjw71XP/+BvE3IP18YYf3ewwEwqhlgaTq6qYD1W3kCYzFjBz0B27chxnE060jysSVTuBwtNi57BsvgpuTKHovL9Ya9Bf0fbcxKXcBhB//uDfYx/gDkVVtQaW9NhODrYXjiJvRVao/g12zHUpmm0XqDfp22WqYR773fel51+PZlNTnGe36fB/ISq3mPhLT0MRT1wIWNlpSeazF1BRzm/8pQ4zAGGlvhSTPbmk4+rTEKsZAPebNFFZby7bnejbrk08gKxQrOF1Y++bDNublsYSuApqLemsFSrbr9+wSINjcbkPssrpTJ1xTruh7wqIX7xb1Rxnt+nwfyEqt5j4S09DEU9ciijFQphNS/MesGkl8B3xQZdCGuncowPu7RtxZJLa9U+EtV2WngFY/jCKu1I2ShljZkBMTc1vsT7gcgXkuvIFHLrDb8DvNgoXThmy6XSR83NM4cTvgwCmju0TjFixTlehQRuNuPJhLpMwnQadxfDZMxVJiKwyHHITOhKN+5ffnauI3P54xpTKTS2XwHuV39FFFZnruC/ZMuaddvfdsoG+OyLRC+aEHskAvQmjgqAn6A1XLgKb00j17b6dgMeDQrBLBR/YCNna3E/eT7CRuDLYmd+q7nDrHty/wWqlj6HIAc3cK74/rPkQ0nc73kZ6aU4gtaO2N4181o7PiA3r2CmUxGEVBU0EuT67I+euvujAmNvEfazqACN+T7zFttAb2wsrf9ESMJG10w4nzLQu4Tay62D0hZM/kAZj058L9617hTxsoZ/Zyb4MrlzcxAjNyH8s=',
-    'loginFlag':'2',
-    'hIsOpenLboAccStatus':'0',
-    'hIsSignLboUserCrsChecked':'0',
-    'userName':username,
-    'pwd':'************',
-    'validNum':captcha,
-    'agreeLbo':'on',
-    'agreeLbo':'on',
-    'agreeLbo':'on',
-    'agreeLbo':'on',
-    'loginagree':'on'
+# ========================== 检查验证码输入是否正确 ===================================
+check_captcha_url = 'https://user.lu.com/user/captcha/pre-check'
+params = {
+    'inputValue':captcha,
+    'source':'login',
+    '_':get_timestamp()
 }
+resp = session.get(check_captcha_url, headers=headers, params=params)
+result = json.loads(resp.text)
+print result
+if result.get('result') == 'SUCCESS':
+	# ========================== 验证码输入正确则进行登录 ====================================
+	login_url = 'https://user.lu.com/user/login'
+	data = {
+	    'isTrust':'Y',
+	    'password':rsa_encrypt(password),
+	    'openlbo':'0',
+	    'deviceKey':'1DA04D5FA06A3DF9351172402D3736E93F5AD7BB4422E6181D17A9C9FE55ABD323517C5B01B09D1254E255600623E57D294B58C7CDAEF4FAA973316CFCAA9CE4E8DD889CF3874E1E9AFFF57FAED8789446D8C69DED5D2EDBAFC73A3B262BF9ED060D16D364AC30720DF3B707A1E81DC89D36822E679D61A3B732A28A02D84678',
+	    'deviceInfo':'ALxyJ/Hzq1cZXHj+WaUCFi8QMlvz0AkQLB8PE6s8VkmRvQy8v9ALWBjtK6pMCjjElX38kes1jGTKpuNvCqZdwQTDF2YVkoL53zgkMEsgO95ZZV1knicKDZ47IZYGXn9neALil2mOa1uOhco5IRMpHT3dJ+JMNg3L1rNEqAGqoX5sQQ51R/R68T22iAMFll6bXUTJTM6Qqlp8ZtijSDwADPEr2bCSlgvmnVcmPdXHXL3nmufjhJ9p7Y44ZuJ/X8MXg6TD77Gdl0DqXd4j70V8X0REudlB4mmvm0uUI8u/kJbjvLLMmpn8lz55bVxHEcFmncEadDFHqYqz31pVb32N4y7/a4bYw5ccPuV6MWX/ZQcdOwGMKQuIvBbbhv70sZZgaO3Qcksp57oPYZFs1zOKYTQDPHHPCmm8HSMq8oauUUfsquo02hA9a47t6A0lDAxuTEQK5RAkdNA3xJyTWfIWqgsDKiRyDQt51jxIZN4DQceUbZBu68xPABxqHUECt07/hiLBrWrQE2O1XzFt3D7WtIMHmAsMe1x/EaLugBMOv7mw4s5cbAjGrL1c5SqfFiTod3NJw2Bo8ZvXMWkpYIV6RtIyvAEaOeY1WMIb+zGei03ZN3Cdwut7mFJ230sPN15QAErZ5Awtax4JUvq4eG2GZoGIUj/sE7Xy7YVhN7p3XGzu+HJw/FJjNBcitE6Mrp8tluTOAOgzcOhFeLEimsAbOTgSyKBaZHZFpJRmRB4WLE0Qh4sQza9jAW+CY7viEF7tRtvkz1pUCjkgFx1al6NxzI39z4iAXrWNZfbsan/9phM27duOeWj6XBXVPgXrtt31LXKCzoSu7GO2ZHxOGRaIi9WKn/FCJkgEYLheORcQuw827duOeWj6XBXVPgXrtt31k7J96vyzYUJtNRgXg47ryaEJnrowXYSOQdhwWm76GvPOSkF31CMkomVId9lumzUSLwb8ABAARkRra79AW/BkAbAGyMZBxFso4AEny9fZnBZpVAuN96/RblkcowPCm5efJX4o+VuMuh7VRvObZFiLdSE4k0yrbPL1UoUNZP4JY4OYzkiMR70mGgxVHLN/vuXyDDo6M8u+96Ch6BkQuYh4jVj/9lwXYMXNbMQwnchIL+QNsZz4slht4C3+gRSQfIZn+TBQnaVv+7BeUIaCW+nIqgvrbG0p5N2X6D/gkq7mN28xReqmLGr4FY60wKtF4PXXHC9qSFkeIBVWg79pGVMG73BTHOTFpcaTKNgK4br7EYgCFjZaUnmsxdQUc5v/KUOM/MYNXC4Jv7cOlA8AtpBZnvco2H1f6IiHp4Z9GtJiHhqXMYUls6CjBFtpyjxk0uVCl0cuoE8CwCPYZWMg1sZz7Ceib5BdMxUTi0gTV7MzMpZXrlQdqdhJKKE1qZD7TE1SEqkzoPv2ll/WZG7juiVIbx9T/ey62uzRIQZXCo5CSqgxReqmLGr4FY60wKtF4PXXOI0qe0YiUYhkEHu+rkAO4aogTj3OKghzLyqw5Bj9qUeNOCQfsZ5fxZ3iZs/4nJRo8KdA3e1vXduJUIWVe3uCyDPf1+pMAsYjiHli7oJWd/ETeHQAAP5fiRiA9JP5nzbyE9EsspJJd2VZnChkkUKdjw71XP/+BvE3IP18YYf3ewwEwqhlgaTq6qYD1W3kCYzFjBz0B27chxnE060jysSVTuBwtNi57BsvgpuTKHovL9Ya9Bf0fbcxKXcBhB//uDfYx/gDkVVtQaW9NhODrYXjiJvRVao/g12zHUpmm0XqDfp22WqYR773fel51+PZlNTnGe36fB/ISq3mPhLT0MRT1wIWNlpSeazF1BRzm/8pQ4zAGGlvhSTPbmk4+rTEKsZAPebNFFZby7bnejbrk08gKxQrOF1Y++bDNublsYSuApqLemsFSrbr9+wSINjcbkPssrpTJ1xTruh7wqIX7xb1Rxnt+nwfyEqt5j4S09DEU9ciijFQphNS/MesGkl8B3xQZdCGuncowPu7RtxZJLa9U+EtV2WngFY/jCKu1I2ShljZkBMTc1vsT7gcgXkuvIFHLrDb8DvNgoXThmy6XSR83NM4cTvgwCmju0TjFixTlehQRuNuPJhLpMwnQadxfDZMxVJiKwyHHITOhKN+5ffnauI3P54xpTKTS2XwHuV39FFFZnruC/ZMuaddvfdsoG+OyLRC+aEHskAvQmjgqAn6A1XLgKb00j17b6dgMeDQrBLBR/YCNna3E/eT7CRuDLYmd+q7nDrHty/wWqlj6HIAc3cK74/rPkQ0nc73kZ6aU4gtaO2N4181o7PiA3r2CmUxGEVBU0EuT67I+euvujAmNvEfazqACN+T7zFttAb2wsrf9ESMJG10w4nzLQu4Tay62D0hZM/kAZj058L9617hTxsoZ/Zyb4MrlzcxAjNyH8s=',
+	    'loginFlag':'2',
+	    'hIsOpenLboAccStatus':'0',
+	    'hIsSignLboUserCrsChecked':'0',
+	    'userName':username,
+	    'pwd':'************',
+	    'validNum':captcha,
+	    'agreeLbo':'on',
+	    'agreeLbo':'on',
+	    'agreeLbo':'on',
+	    'agreeLbo':'on',
+	    'loginagree':'on'
+	}
 
-resp = session.post(login_url, headers=headers, data=data)
-print resp.text
+	resp = session.post(login_url, headers=headers, data=data)
+	print resp.text
